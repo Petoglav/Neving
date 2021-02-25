4dccef4101fb8e33eccd8bb42e40aed8dc897249
Petoglav <mislav.mat@hotmail.com>
2021-02-25 22:27:26 +0100

    Slider, counter, about us

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
-var rButton = document.querySelector('.rButton');
 
+var eButton = document.querySelector('.eButton');
+eButton.onclick = function(e) {
+  e.preventDefault(), $(this).next("ul").slideToggle(), $(this).toggleClass("expanded")
+}
+
+var rButton = document.querySelector('.rButton');
 rButton.onclick = function(e) {
     e.preventDefault(),
     "yes" == $(this).data("expanded") ? ($(".izbornik").slideUp(),
@@ -25,4 +30,43 @@ function scrollFunction() {
 }
 $('#myBtn').click(function(){
     window.scroll({top: 0, behavior: "smooth"})
+});
+
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" trenutna", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " trenutna";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+var stop = $(".zabrojilo").offset().top;
+$(window).scroll(function() {
+    if ($(window).scrollTop() > stop ) {
+        $(window).off("scroll");
+        $('.broj').each(function () {
+            var $this = $(this);
+            jQuery({ Counter: $this.attr("data-decimal") }).animate({ Counter: $this.attr("data-number") }, {
+                duration: 4000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+    }
 });
